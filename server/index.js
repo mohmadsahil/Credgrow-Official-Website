31ac2ca2c015534a9537f60b899b29b43a4f852c
Mohmad Sahil <sahilsid2001@gmail.com>
2024-06-09 13:42:05 +0530

    Production Ready

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 const app = express();
 const PORT = 8000;
+import path from "path";
 
+// app.get("/",(req,res)=>{
+//     res.send("Server is Activated");
+// })
 
-
-app.get("/",(req,res)=>{
-    res.send("Server is Activated");
-})
+const _dirname = path.dirname("")
+const buildpath = path.join(_dirname,"../client/build")
+app.use(express.static(buildpath));
 
 app.listen(PORT,(req,res)=>{
     console.log(`App is Listening on PORT ${PORT}`);
